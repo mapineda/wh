a9e115059555d260b9b3b90784bd1e90bff06f31
mapineda Pineda <mapineda1222@gmail.com>
2015-11-09 17:23:58 -0600

    update

--- node-postgres-wh/routes/index.js
+++ node-postgres-wh/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+//adding pg connects
+// var pg = require('pg');
+// var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
