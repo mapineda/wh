3bacbee3ec0f122876da25e79c76a19d46f2c5d6
mapineda Pineda <mapineda1222@gmail.com>
2015-11-12 04:08:23 -0600

    great success with about routes

--- node-postgres-wh/app.js
+++ node-postgres-wh/app.js
@@ -20,7 +20,7 @@ var personality_insights = watson.personality_insights({
   version: 'v2'
 });    
 
-var about = require('.routes/about');
+var about = require('./routes/about');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -49,8 +49,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/about', about);
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
